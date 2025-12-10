f1526b8037d236ea048fbd1d452b73f41b316a52
Gowtham <gowthamkollapudi123@gmail.com>
2025-12-10 12:29:53 +0530

    feat: Implement initial Flutter app with home screen navigation to a new screen displaying an image and buttons.

--- lib/scrren_one.dart
+++ lib/scrren_one.dart
@@ -24,7 +24,7 @@ class ScreenOne extends StatelessWidget {
                   child: Image.network(
                     'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=200&q=80',
                     width: 100,
-                    height: 100,
+                    height: 105,
                     fit: BoxFit.cover,
                     loadingBuilder: (context, child, loadingProgress) {
                       if (loadingProgress == null) return child;
